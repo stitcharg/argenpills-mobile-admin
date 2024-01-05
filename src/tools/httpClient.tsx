d06928ddcf3fedc973d34ce73e170cd0011c2b2c
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-01-05 11:30:27 -0300

    Agregar soporte para UTF-8

--- src/tools/httpClient.tsx
+++ src/tools/httpClient.tsx
@@ -2,7 +2,7 @@ import { fetchUtils } from "react-admin";
 
 const httpClient = async (url, options = {}) => {
   if (!options.headers) {
-    options.headers = new Headers({ Accept: 'application/json' });
+    options.headers = new Headers({ Accept: 'application/json; charset=utf-8' });
   }
   const token = localStorage.getItem('token');
   options.headers.set('Authorization', `Bearer ${token}`);
