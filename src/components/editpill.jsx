3559825d1eeb3fe8be717fc2d0f38124e44a9c01
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-01-08 18:48:18 -0300

    Editar un item con una imagen y agregarle otra hace que la primera imagen se pierda (#15)
    * Work in progress (no anda bien todavia)
    
    * Borra la imagen (al fin), falta la del lab
    
    * Arreglado el upload de imagenes
    
    * Borrado el estilo vacio
    
    * ESPACIOS?? Me arrugan la ropa

--- src/components/editpill.jsx
+++ src/components/editpill.jsx
@@ -1,11 +1,94 @@
+
+import { useState } from 'react';
 import { Edit, SelectInput, SimpleForm, ImageField, ImageInput, FormDataConsumer, Labeled, BooleanInput } from 'react-admin';
-import { DateInput, TextInput, required } from 'react-admin';
+import { DateInput, TextInput, required, Button } from 'react-admin';
+import { FormValuesDisplay } from './formvaluesdisplay';
+import { useWatch, useFormContext } from 'react-hook-form';
+
+const IMAGE_TYPE = {
+	PILL: 0,
+	LAB: 1
+}
 
 export const PillEdit = props => {
-	const transform = data => ({
-		...data,
-		published: true
-	});
+	const [newImageUploaded, setNewImageUploaded] = useState(false);
+	const [newLabImageUploaded, setNewLabImageUploaded] = useState(false);
+
+	const transform = data => {
+		let transformedData = {
+			...data,
+			newImageUploaded: newImageUploaded,
+			newLabImageUploaded: newLabImageUploaded,
+			published: true
+		};
+
+		if (transformedData.image === null) delete transformedData.image;
+		if (transformedData.lab_image === null) delete transformedData.lab_image;
+
+		if (transformedData.upl_image === null) delete transformedData.upl_image;
+		else if (newImageUploaded) delete transformedData.image;
+
+		if (transformedData.upl_lab_image === null) delete transformedData.upl_lab_image;
+		else if (newLabImageUploaded) delete transformedData.lab_image;
+
+		return transformedData;
+	};
+
+	const ClearImageButton = ({ imageType, ...formDataProps }) => {
+		const formContext = useFormContext();
+
+		let controlName = '';
+		let existingImage = '';
+		switch (imageType) {
+			case IMAGE_TYPE.PILL: {
+				controlName = 'image';
+				existingImage = formDataProps.formData.image;
+				break;
+			}
+			case IMAGE_TYPE.LAB: {
+				controlName = 'lab_image';
+				existingImage = formDataProps.formData.lab_image;
+				break;
+			}
+		}
+
+		const handleClearImage = (field) => {
+			formContext.setValue(field, null);
+		};
+
+		return (
+			(existingImage != null ?
+				<Button onClick={() => handleClearImage(controlName)} label='Borrar'></Button> : null)
+		);
+	};
+
+	const ImageDisplay = ({ imageType }) => {
+		const formContext = useFormContext();
+
+		let controlName = '';
+		switch (imageType) {
+			case IMAGE_TYPE.PILL: {
+				controlName = 'image';
+				break;
+			}
+			case IMAGE_TYPE.LAB: {
+				controlName = 'lab_image';
+				break;
+			}
+		}
+
+		const imageValue = useWatch({
+			name: controlName,
+			control: formContext.control,
+		});
+
+		if (imageValue === null) return <></>
+
+		return (
+			<Labeled label="Foto existente">
+				<ImageField source={controlName} />
+			</Labeled>);
+	};
 
 	return (
 		<Edit {...props} title="Editar informacion" redirect="list" transform={transform}>
@@ -33,40 +116,46 @@ export const PillEdit = props => {
 					{ id: 2, name: 'Peligrosa' },
 				]} helperText="Si no tiene advertencias, elegir Sin alerta" />
 
-				<ImageInput source="upl_image" maxSize={1024000} accept="image/*" fullWidth={false} label="Foto de la pastilla">
+				<ImageInput
+					source="upl_image"
+					maxSize={1024000}
+					accept="image/*"
+					fullWidth={false} label="Foto de la pastilla"
+					onChange={() => setNewImageUploaded(true)}>
+
 					<ImageField source="src"></ImageField>
 				</ImageInput>
 
 				<FormDataConsumer>
-					{({ formData, dispatch, ...rest }) => {
-						if (!formData.upl_image && formData.image) {
-							return (
-								<div>
-									<Labeled label="Foto existente">
-										<ImageField source="image" {...rest} />
-									</Labeled>
-								</div>
-							);
-						}
-					}}
+					{formDataProps => (
+						<>
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<ImageDisplay imageType={IMAGE_TYPE.PILL} />
+								<ClearImageButton {...formDataProps} imageType={IMAGE_TYPE.PILL} />
+							</div>
+						</>
+					)}
 				</FormDataConsumer>
 
-				<ImageInput source="upl_lab_image" maxSize={1024000} accept="image/*" fullWidth={false} label="Foto del test" >
+				<ImageInput
+					source="upl_lab_image"
+					maxSize={1024000}
+					accept="image/*"
+					fullWidth={false} label="Foto del test"
+					onChange={() => setNewLabImageUploaded(true)}>
+
 					<ImageField source="src"></ImageField>
 				</ImageInput>
 
 				<FormDataConsumer>
-					{({ formData, dispatch, ...rest }) => {
-						if (!formData.upl_lab_image && formData.lab_image) {
-							return (
-								<div>
-									<Labeled label="Foto existente">
-										<ImageField source="lab_image" {...rest} />
-									</Labeled>
-								</div>
-							);
-						}
-					}}
+					{formDataProps => (
+						<>
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<ImageDisplay imageType={IMAGE_TYPE.LAB} />
+								<ClearImageButton {...formDataProps} imageType={IMAGE_TYPE.LAB} />
+							</div>
+						</>
+					)}
 				</FormDataConsumer>
 
 				<TextInput source="lab_url" fullWidth={true} label="URL del test" autoComplete="off" />
@@ -76,6 +165,7 @@ export const PillEdit = props => {
 				<TextInput source="ap_url" label="Argenpills URL" fullWidth={true} validate={required()} autoComplete="off" />
 
 				<BooleanInput label="Publicada" source="published" disabled={true} />
+
 			</SimpleForm>
 		</Edit>);
 };
---
+++ src/components/formvaluesdisplay.jsx
@@ -0,0 +1,7 @@
+import { FormDataConsumer } from "react-admin";
+
+export const FormValuesDisplay = () => (
+    <FormDataConsumer>
+        {({ formData }) => <pre>{JSON.stringify(formData, null, 2)}</pre>}
+    </FormDataConsumer>
+);
