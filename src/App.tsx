c861c28920d6572badbef59d3e5c05f6a29bfda3
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-01-08 18:13:00 -0300

    Borrado el estilo vacio

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import { APDataProvider } from './providers/dataprovider';
 import authProvider from './providers/authprovider';
 import dashboard from './pages/dashboard';
 
-import './styles/custom.css';
-
 export function App() {
 
     return (
